dd3aef8547c2d408b4843def67b4c5e42fd9d05a
Ruslan Opihaylenko <rysya08shkolnik@gmail.com>
2025-12-17 18:11:15 +0200

    add modules

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import Card from "./components/Card";
+import styles from'./index.module.css'
 
 const App = () => {
   return (
@@ -7,11 +8,11 @@ const App = () => {
       <Card width="1500px" height="500px" variant={"primary"} 
       >
         <br />
-        <h1>Книжки для малечі</h1>
-        <h2 className="first">Обирайте 3 книжки за ціною двох для свого малюка!</h2>
-        <h2 className="second">Збирайте свою бібліотеку яскравого настрою!</h2>
-        <button style={{ marginTop: "20px" }}>Детальніше</button>
-        <img src="https://www.ranok.com.ua/storage/images/VRPwWsctM3T0JQpeLFJuLqyAhQppKmqokH7TMBtA.jpg"></img>
+        <h1 className={`${styles.title}`}>Книжки для малечі</h1>
+        <h2 className={`${styles.first}`}>Обирайте 3 книжки за ціною двох для свого малюка!</h2>
+        <h2 className={`${styles.second}`}>Збирайте свою бібліотеку яскравого настрою!</h2>
+        <button className={`${styles.button_size}`} style={{ marginTop: "20px" }}>Детальніше</button>
+        <img className={`${styles.image}`} src="https://www.ranok.com.ua/storage/images/VRPwWsctM3T0JQpeLFJuLqyAhQppKmqokH7TMBtA.jpg"></img>
       </Card>
     </>
   );
